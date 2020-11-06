837e0174ce3ecd8e72acb88186279c0cd2138f07
RuneAegyr <rune.aegyr@gmail.com>
2020-11-06 21:12:37 +0700

    looping

--- Tugas4_123200119.cpp
+++ Tugas4_123200119.cpp
@@ -17,16 +17,16 @@ int main()
 			cout << "Input angka M : "; cin >> M;
 			cout << "Hasil " << N << " X " << M << endl;
 			
-			n = N;
+			n = M;
 			m = 0;
 			
-			while(m<=M){
+			while(m<=N){
 				m++;
-				if(m<M){
-					n = N + n;
-					cout << N << " x ";
-				} else if(m == M){
-					cout << N << " = " << n << endl;
+				if(m<N){
+					n = M + n;
+					cout << M << " x ";
+				} else if(m == N){
+					cout << M << " = " << n << endl;
 				}
 			}
 		break;
@@ -56,3 +56,5 @@ int main()
 
 	return 0;
 }
+
+//123200119 - Dentang Canang Wardhana
